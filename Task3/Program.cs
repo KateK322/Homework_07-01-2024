2ab120482818ae19464dcbe2adfd14f6a5792290
Ekaterina <kononova.22@mail.ru>
2024-01-07 02:01:24 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"случайное число из отрезка [10, 99]={number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+}
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; 
+Console.WriteLine(maxDigit);
+
+
